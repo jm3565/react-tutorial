fb0e32d84d1573881972210b1fff53ceadfef084
Juan <jnmunoz74@gmail.com>
2021-02-21 21:44:12 -0500

    Rewrite Board to use two loops to make the squares instead of hardcoding them.

--- src/components/board.js
+++ src/components/board.js
@@ -8,21 +8,15 @@ const Board = ({ squares, onClick }) => {
 
   return (
     <div>
-      <div className="board-row">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-      </div>
-      <div className="board-row">
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div className="board-row">
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
+      {Array(3)
+        .fill(null)
+        .map((v, i) => (
+          <div className="board-row">
+            {Array(3)
+              .fill(null)
+              .map((v, j) => renderSquare(j + i * 3))}
+          </div>
+        ))}
     </div>
   );
 };
